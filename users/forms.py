4ee7ca08a460d2b17361ec608f89f1415cb2391e
Andrey Yablokov <AndrewYablokov@yandex.ru>
2021-10-18 10:15:31 +0300

    1. Создал модель пользователя в проекте. Выполнил настройки в файле конфигурации. 2. Реализовал механизм аутентификации и авторизации в проекте. 3. Реализовал механизм регистрации пользователя. Добавил logout. 4. Создал base.html для login.html и register.html в templates папке приложения users. 5. Создал свой валидатор Age.

---
+++ users/forms.py
@@ -0,0 +1,61 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django import forms
+
+from users.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Введите имя пользователя',
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Введите пароль',
+    }))
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+
+class UserRegistrationForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Введите имя пользователя',
+    }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Введите адрес эл. почты',
+    }))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Введите имя',
+    }))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Введите фамилию',
+    }))
+    age = forms.IntegerField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Введите возраст',
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Введите пароль',
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Подтвердите пароль',
+    }))
+
+    class Meta:
+        model = User
+        fields = {'username', 'email', 'first_name', 'last_name', 'password1', 'password2'}
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+        if data < 18:
+            raise forms.ValidationError("Вы слишком молоды для покупок в нашем магазине!")
+
+        return data
