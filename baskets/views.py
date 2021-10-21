60669f8f414462814e5539ba013b7c234d3d85bb
Andrey Yablokov <AndrewYablokov@yandex.ru>
2021-10-21 22:41:35 +0300

    Реализовал вывод товаров корзины.

--- baskets/views.py
+++ baskets/views.py
@@ -5,11 +5,12 @@ from baskets.models import Basket
 
 
 def basket_add(request, product_id):
+    user = request.user
     product = Product.objects.get(id=product_id)
-    baskets = Basket.objects.filter(user=request.user, product=product)
+    baskets = Basket.objects.filter(user=user, product=product)
 
     if not baskets.exists():
-        Basket.objects.create(user=request.user, product=product, quantity=1)
+        Basket.objects.create(user=user, product=product, quantity=1)
         return HttpResponseRedirect(request.META['HTTP_REFERER'])
     else:
         basket = baskets.first()
