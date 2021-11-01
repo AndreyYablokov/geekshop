7aa8850129cfc04f44d44580967c3d971c5d753b
Andrey Yablokov <AndrewYablokov@yandex.ru>
2021-11-01 08:38:03 +0300

    Организовал фильтрацию продуктов по категориям.

--- products/views.py
+++ products/views.py
@@ -16,12 +16,22 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
+def products(request, category_id=None):
     context = {
         'title': 'GeekShop - Каталог',
         'header': 'GeekShop',
-        'categories': ProductCategory.objects.all(),
-        'products': Product.objects.all(),
     }
+
+    categories = []
+    for category in ProductCategory.objects.all():
+        if Product.objects.filter(category=category):
+            categories.append(category)
+    context['categories'] = categories
+        
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+    context['products'] = products
 
     return render(request, 'products/products.html', context)
