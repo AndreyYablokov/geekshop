40cd0270538528fc31dabadf987b5087825a0d63
Andrey Yablokov <AndrewYablokov@yandex.ru>
2021-11-08 16:40:04 +0300

    Исправил модель пользователя

--- users/models.py
+++ users/models.py
@@ -10,11 +10,10 @@ class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True, null=True)
     age = models.PositiveIntegerField(default=40)
     activation_key = models.CharField(max_length=128, blank=True)
-    activation_key_expires = models.DateTimeField(default=(now() + timedelta(hours=48)))
 
     def safe_delete(self):
         self.is_active = False
         self.save()
 
     def is_activation_key_expired(self):
-        return now() > self.activation_key_expires
+        return now() - self.date_joined > timedelta(hours=48)
